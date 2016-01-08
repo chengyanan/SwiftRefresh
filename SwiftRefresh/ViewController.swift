1fdca3f2ea8e9c541c33e532f0d1bff9b1630a61
chengyanan <1914293706@qq.com>
2016-01-08 10:15:42 +0800

    Initial Commit

---
+++ SwiftRefresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftRefresh
+//
+//  Created by 农盟 on 16/1/8.
+//  Copyright © 2016年 农盟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
